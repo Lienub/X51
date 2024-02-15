a75ef403e0849700a1c14ead7f7a85cee46fa223
Benoît Naegel <bnaegel@morpho.u-strasbg.fr>
2024-02-15 08:40:54 +0100

    TP2

--- base/filtres.cpp
+++ base/filtres.cpp
@@ -1,3 +1,24 @@
 #include "filtres.h"
 
-Image <double> convolution(const Image <uint8_t> &image, const Image <double> &masque);
+Image<uint8_t> convert_double_to_uint8(const Image<double> &image){
+    Image<uint8_t> res(image.getDx(), image.getDy());
+    return res;
+}
+
+
+
+double somme_ponderee(const Image <uint8_t> &image, const Image <double> &masque, int x, int y) 
+{
+    return 0.0;
+}
+
+Image <double> convolution(const Image <uint8_t> &image, const Image <double> &masque)
+{
+    return Image <double> (image.getDx(), image.getDy());
+
+}
+
+Image <double> masque_gaussien( double sigma)
+{
+    return Image <double> (1, 1);
+}
--- base/filtres.h
+++ base/filtres.h
@@ -3,6 +3,13 @@
 
 #include "image.h"
 
+
+Image<uint8_t> convert_double_to_uint8(const Image<double> &image);
+
+double somme_ponderee(const Image <uint8_t> &image, const Image <double> &masque, int x, int y);
+
 Image <double> convolution(const Image <uint8_t> &image, const Image <double> &masque);
+
+Image <double> masque_gaussien( double sigma);
 
 #endif
