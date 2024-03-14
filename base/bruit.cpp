74a583f239a6f97e0c1d013bc80f5ac6e7d4389e
Julien Abid <lienub.68@gmail.com>
2024-03-14 15:46:51 +0100

    bruits

--- base/bruit.cpp
+++ base/bruit.cpp
@@ -4,6 +4,21 @@
 Image <uint8_t> bruit_impulsionnel(const Image<uint8_t> &img, double p)
 {
     Image <uint8_t> res(img);
+
+    for (int i = 0; i< img.getDx(); i++) {
+        for (int j = 0 ; j < img.getDy() ; j++ ) {
+            double pick = (double)rand() / RAND_MAX;
+
+            if (pick > 0 && pick > p) {
+                double r = (double)rand() / RAND_MAX;
+                if(r < 0.5) {
+                    res(i,j) = 0;
+                } else {
+                    res(i,j) = 255;
+                }
+            }            
+        }
+    }
     
    
 
@@ -14,6 +29,26 @@ Image <uint8_t> bruit_gaussien(const Image<uint8_t> &img, double sigma)
     std::default_random_engine generator;
 
     Image <uint8_t> res(img);
-    
+
+    std::normal_distribution<double> distribution(0.0, sigma);
+
+    for (int i = 0; i< img.getDx(); i++) {
+        for (int j = 0 ; j < img.getDy() ; j++ ) {
+            // Generation d'un nombre aléatoire suivant la loi de la probabilité gaussienne
+            double r = distribution(generator);
+
+            double v = img(i,j) + r;
+
+            // On tronque l'image
+            if (v < 0) {
+                res(i,j) = 0;
+            } else if (v > 255) {
+                res(i,j) = 255;
+            } else {
+                res(i,j) = v;
+            }
+        }
+    }
+
     return res;
 }
